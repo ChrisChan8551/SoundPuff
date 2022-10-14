201a51fa8088aa43822445ba09d34168dd3af0ca
ChrisC <chrischan8551@gmail.com>
2022-10-13 23:03:29 -0500

    create playlist

--- backend/routes/api/playlists.js
+++ backend/routes/api/playlists.js
@@ -1,6 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const { setTokenCookie, requireAuth } = require('../../utils/auth.js');
+const {
+	User,
+	Song,
+	Comment,
+	Playlist,
+	Album,
+	PlaylistSong,
+} = require('../../db/models');
 
-const { Playlist } = require('../../db/models');
+//Create a playlist
+router.post('/', requireAuth, async (req, res) => {
+	const { name, imageUrl } = req.body;
+
+	if (!name) {
+		return res.status(400).json({
+			message: 'Validation Error',
+			statusCode: 400,
+			errors: {
+				name: 'Playlist name is required',
+			},
+		});
+	}
+	const newPlaylist = await Playlist.create({
+		userId: req.user.id,
+		name,
+		imageUrl: imageUrl,
+	});
+
+	return res.status(201).json(newPlaylist);
+});
 
 module.exports = router;
