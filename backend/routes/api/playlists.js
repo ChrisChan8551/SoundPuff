5c3236185d9128a47b9ac136c42547779075b827
Chris Chan <chrischan8551@gmail.com>
2022-10-14 13:06:24 -0500

    add song to playlist

--- backend/routes/api/playlists.js
+++ backend/routes/api/playlists.js
@@ -10,6 +10,60 @@ const {
 	PlaylistSong,
 } = require('../../db/models');
 
+// Add a Song to a Playlist based on the Playlists's id
+router.post('/:playlistId/songs',
+    requireAuth,
+    async (req, res) => {
+        const { songId } = req.body;
+        const { playlistId } = req.params;
+        const song = await Song.findByPk(songId);
+        const onePlaylist = await Playlist.findByPk(playlistId);
+
+        if(!song){
+            res.status(404);
+            return res.json({ "message": "Song couldn't be found", "statusCode": 404 });
+        }
+        if(!onePlaylist){
+            res.status(404);
+            return res.json({ "message": "Playlist couldn't be found", "statusCode": 404 });
+        }
+
+        if(onePlaylist.userId === req.user.id){
+            await onePlaylist.addSong(song);
+
+            const newSong = await PlaylistSong.findOne({
+                where: { songId },
+                order: [['createdAt', 'DESC']],
+                attributes: ['id', 'playlistId', 'songId']
+            });
+
+            return res.status(200).json(newSong);
+        } else {
+            return res.json({"message": "You can only add a song to a playlist if you are the playlist owner"});
+        }
+});
+
+// Get details of a Playlist from an id
+router.get('/:playlistId', async (req, res, next) => {
+    const { playlistId } = req.params;
+    const playlist = await Playlist.findOne({
+        where: { id: playlistId},
+        include: [{ model: Song, through: {
+            attributes: []}
+        }]
+    });
+
+    if(!playlist){
+        return res.status(404).json({
+            "message": "Playlist couldn't be found",
+            "statusCode": 404
+        });
+    }
+
+    return res.json(playlist);
+});
+
+
 //Create a playlist
 router.post('/', requireAuth, async (req, res) => {
 	const { name, imageUrl } = req.body;
@@ -31,5 +85,7 @@ router.post('/', requireAuth, async (req, res) => {
 
 	return res.status(201).json(newPlaylist);
 });
+
+
 
 module.exports = router;
