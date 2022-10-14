182ae173336d6854cf01bcddc32c18e66cbaf64e
ChrisC <chrischan8551@gmail.com>
2022-10-13 22:46:35 -0500

    delete comment

--- backend/routes/api/comments.js
+++ backend/routes/api/comments.js
@@ -12,6 +12,58 @@ const {
 	PlaylistSong,
 } = require('../../db/models');
 
+//Delete a comment
+router.delete('/:commentId', requireAuth, async (req, res) => {
+	const { commentId } = req.params;
+	const deleteComment = await Comment.findByPk(commentId);
 
+	if (!deleteComment) {
+		return res
+			.status(404)
+			.json({ message: "Comment couldn't be found", statusCode: 404 });
+	}
+
+	if (deleteComment.userId === req.user.id) {
+		await deleteComment.destroy();
+		return res.json({ message: 'Successfully deleted', statusCode: 200 });
+	} else {
+		return res.json({
+			message: 'A comment can only be deleted by the comment owner',
+		});
+	}
+});
+
+// Edit a Comment
+router.put('/:commentId', requireAuth, async (req, res) => {
+	const { body } = req.body;
+	const { commentId } = req.params;
+	const updateComment = await Comment.findByPk(commentId);
+
+	if (!updateComment) {
+		return res
+			.status(404)
+			.json({ message: "Comment couldn't be found", statusCode: 404 });
+	}
+
+	if (!body) {
+		return res.status(400).json({
+			message: 'Validation Error',
+			statusCode: 400,
+			errors: {
+				body: 'Comment body text is required',
+			},
+		});
+	}
+	if (updateComment.userId === req.user.id) {
+		updateComment.update({
+			body,
+		});
+		return res.json(updateComment);
+	} else {
+		return res.json({
+			message: 'A comment can only be edited by the comment owner',
+		});
+	}
+});
 
 module.exports = router;
