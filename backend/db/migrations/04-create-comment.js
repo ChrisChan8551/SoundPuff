3ffeebb6b617ba39983bc008a64d6a813e034190
ChrisC <chrischan8551@gmail.com>
2022-11-16 22:37:33 -0600

    1

--- backend/db/migrations/04-create-comment.js
+++ backend/db/migrations/04-create-comment.js
@@ -1,6 +1,6 @@
 'use strict';
 let options = {};
-options.tableName = 'Comments';
+
 
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
