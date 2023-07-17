196611e33034614b73428c1c1dc73cfbc1eae410
ChrisC <chrischan8551@gmail.com>
2023-07-16 21:10:06 -0500

    carousel

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,12 +1,36 @@
+import React from 'react';
 import './HomePage.css';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 function HomePage() {
+	const images = [
+		'https://a-v2.sndcdn.com/assets/images/sc_landing_header_web_b-447230ef.jpg',
+		'https://a-v2.sndcdn.com/assets/images/sc_landing_header_web_a-3bb2d420.jpg',
+		'https://a-v2.sndcdn.com/assets/images/sc_landing_header_web_c-318d7eed.jpg',
+		'https://a-v2.sndcdn.com/assets/images/sc_landing_header_web_featured_artists-8081257b.jpg',
+	];
+
+	const settings = {
+		dots: true,
+		infinite: true,
+		speed: 1000,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 4000,
+	};
+
 	return (
 		<div className='home-container'>
-			<img
-				src='https://a-v2.sndcdn.com/assets/images/sc_landing_header_web_b@2x-7e5ff471.jpg'
-				alt='home pic'
-			></img>
+			<Slider {...settings}>
+				{images.map((imageUrl, index) => (
+					<div key={index}>
+						<img src={imageUrl} alt='' />
+					</div>
+				))}
+			</Slider>
 		</div>
 	);
 }
